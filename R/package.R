9f878e1d562a4b083a9cb2b336c8074dc6e0edd5
Mark Klik <mklik@nza.nl>
2019-08-27 12:06:59 +0200

    Import packageVersion from utils

---
+++ R/package.R
@@ -0,0 +1,24 @@
+#  syntheticbench - R package for benchmarking of dataset serialization
+#
+#  Copyright (C) 2019-present, Mark AJ Klik
+#
+#  This file is part of the lazyvec R package.
+#
+#  The lazyvec R package is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  The lazyvec R package is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License along
+#  with the lazyvec R package. If not, see <http://www.gnu.org/licenses/>.
+#
+#  You can contact the author at:
+#  - syntheticbench R package source repository : https://github.com/fstpackage/syntheticbench
+
+
+#' @importFrom utils packageVersion
+NULL
