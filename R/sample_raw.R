ef282e20bc27e806ed4e389b8b8f6f9b2f121a70
markklik <markklik@gmail.com>
2019-09-12 10:43:45 +0200

    Add method sample_raw (fixes #16)

---
+++ R/sample_raw.R
@@ -0,0 +1,54 @@
+#  syntheticbench - R package for benchmarking of dataset serialization
+#
+#  Copyright (C) 2019-present, Mark AJ Klik
+#
+#  This file is part of the lazyvec R package.
+#
+#  The lazyvec R package is free software: you can redistribute it and/or modify it
+#  under the terms of the GNU Affero General Public License version 3 as
+#  published by the Free Software Foundation.
+#
+#  The lazyvec R package is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+#  FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+#  for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License along
+#  with the lazyvec R package. If not, see <http://www.gnu.org/licenses/>.
+#
+#  You can contact the author at:
+#  - syntheticbench R package source repository : https://github.com/fstpackage/syntheticbench
+
+
+#' Generate a raw vector with custom distribution
+#'
+#' @param length length of the vector
+#' @param max_distict_values maximum number of disctict values in the vector
+#' @param min_value minimum value in the returned raw vector
+#' @param max_value maximum value in the returned raw vector
+#'
+#' @return raw vector
+#' @export
+sample_raw <- function(length, min_value = 0, max_value = 255,
+  max_distict_values = NULL) {
+
+  if (min_value < 0) {
+    stop("Parameter min_value should be equal to or larger than 0")
+  }
+
+  if (max_value > 255) {
+    stop("Parameter max_value should be equal to or smaller than 255")
+  }
+
+  if (is.null(max_distict_values)) {
+    return(as.raw(sample(min_value:max_value, length, replace = TRUE)))
+  }
+
+  if (max_distict_values > 256) {
+    stop("Parameter max_distinct_values should be in the range 1 to 256 or NULL")
+  }
+  
+  max_distict_values <- min(max_distict_values, 1 + max_value - min_value)
+  x <- sample(min_value:max_value, max_distict_values)  # unique values
+  as.raw(sample(x, length, replace = TRUE))
+}
---
+++ tests/testthat/test_sample_raw.R
@@ -0,0 +1,29 @@
+
+require(testthat)
+
+context("sample raw")
+
+
+test_that("sample raw", {
+  x <- sample_raw(10)
+
+  expect_equal(length(x), 10)
+  expect_type(x, "raw")
+
+  # set min
+  x <- sample_raw(10, min_value = 250)
+  expect_true(sum(as.integer(x) < 250) == 0)
+
+  # set max
+  x <- sample_raw(10, max_value = 2)
+  expect_true(sum(as.integer(x) > 2) == 0)
+
+  # set min and max
+  x <- sample_raw(10, 10, 12)
+  expect_true(sum(as.integer(x) < 10) == 0)
+  expect_true(sum(as.integer(x) > 12) == 0)
+  
+  # set distinct values
+  x <- sample_raw(10, max_distict_values = 2)
+  expect_true(length(unique(x)) <= 2)
+})
